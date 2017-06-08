f977af6a83a525390b878789746006b4d4549d69
childressdev <jcampbell@childressagency.com>
2017-06-08 14:26:46 -0400

    added home template and quote template part

--- home.php
+++ home.php
@@ -2,102 +2,28 @@
 <main id="main">
   <div class="container">
     <?php get_template_part('partials/main-section-title'); ?>
-    <div class="loop-item">
-      <div class="row">
-        <div class="col-sm-4">
-          <img src="images/blog-placeholder.jpg" class="img-responsive center-block" alt="" />
-        </div>
-        <div class="col-sm-8">
-          <div class="loop-item-group">
-            <div class="loop-item-header">
-              <h2><a href="#">New barrel design for further, more accurate shots</a></h2>
-              <h4>March 24, 2017</h4>
-            </div>
-            <hr />
-            <div class="loop-item-content">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at tempus lorem, id vestibulum turpis. Praesent bibendum scelerisque nisi, at fermentum diam. Donec lacinia metus id sem feugiat, at tempus ligula mollis. Sed elit purus, iaculis non odio id, malesuada ullamcorper ipsum. Nulla finibus nunc in felis sagittis, vel tincidunt libero rhoncus. In id eleifend eros. Aliquam sit amet nunc eget sem posuere scelerisque eget sit amet massa.</p>
-              <p>Nulla fermentum tortor a mi viverra, at maximus ex lobortis. Vestibulum eget efficitur sem. Aenean ultricies lorem ligula, ac finibus quam sagittis maximus. In ultrices, sapien fermentum rutrum condimentum, erat ligula gravida ante, at bibendum purus nisl at </p>
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+      <div class="loop-item">
+        <div class="row">
+          <div class="col-sm-4">
+            <img src="<?php echo get_field('featured_image') ? get_field('featured_image') : get_stylesheet_directory_uri() . '/images/blog-placeholder.jpg'; ?>" class="img-responsive center-block" alt="" />
+          </div>
+          <div class="col-sm-8">
+            <div class="loop-item-group">
+              <div class="loop-item-header">
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <h4><?php echo get_the_date('F j, Y'); ?></h4>
+              </div>
+              <hr />
+              <div class="loop-item-content">
+                <?php the_excerpt(); ?>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="loop-item">
-      <div class="row">
-        <div class="col-sm-4">
-          <img src="images/blog-placeholder.jpg" class="img-responsive center-block" alt="" />
-        </div>
-        <div class="col-sm-8">
-          <div class="loop-item-group">
-            <div class="loop-item-header">
-              <h2><a href="#">New barrel design for further, more accurate shots</a></h2>
-              <h4>March 24, 2017</h4>
-            </div>
-            <hr />
-            <div class="loop-item-content">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at tempus lorem, id vestibulum turpis. Praesent bibendum scelerisque nisi, at fermentum diam. Donec lacinia metus id sem feugiat, at tempus ligula mollis. Sed elit purus, iaculis non odio id, malesuada ullamcorper ipsum. Nulla finibus nunc in felis sagittis, vel tincidunt libero rhoncus. In id eleifend eros. Aliquam sit amet nunc eget sem posuere scelerisque eget sit amet massa.</p>
-              <p>Nulla fermentum tortor a mi viverra, at maximus ex lobortis. Vestibulum eget efficitur sem. Aenean ultricies lorem ligula, ac finibus quam sagittis maximus. In ultrices, sapien fermentum rutrum condimentum, erat ligula gravida ante, at bibendum purus nisl at </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="loop-item">
-      <div class="row">
-        <div class="col-sm-4">
-          <img src="images/blog-placeholder.jpg" class="img-responsive center-block" alt="" />
-        </div>
-        <div class="col-sm-8">
-          <div class="loop-item-group">
-            <div class="loop-item-header">
-              <h2><a href="#">New barrel design for further, more accurate shots</a></h2>
-              <h4>March 24, 2017</h4>
-            </div>
-            <hr />
-            <div class="loop-item-content">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at tempus lorem, id vestibulum turpis. Praesent bibendum scelerisque nisi, at fermentum diam. Donec lacinia metus id sem feugiat, at tempus ligula mollis. Sed elit purus, iaculis non odio id, malesuada ullamcorper ipsum. Nulla finibus nunc in felis sagittis, vel tincidunt libero rhoncus. In id eleifend eros. Aliquam sit amet nunc eget sem posuere scelerisque eget sit amet massa.</p>
-              <p>Nulla fermentum tortor a mi viverra, at maximus ex lobortis. Vestibulum eget efficitur sem. Aenean ultricies lorem ligula, ac finibus quam sagittis maximus. In ultrices, sapien fermentum rutrum condimentum, erat ligula gravida ante, at bibendum purus nisl at </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="loop-item">
-      <div class="row">
-        <div class="col-sm-4">
-          <img src="images/blog-placeholder.jpg" class="img-responsive center-block" alt="" />
-        </div>
-        <div class="col-sm-8">
-          <div class="loop-item-group">
-            <div class="loop-item-header">
-              <h2><a href="#">New barrel design for further, more accurate shots</a></h2>
-              <h4>March 24, 2017</h4>
-            </div>
-            <hr />
-            <div class="loop-item-content">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at tempus lorem, id vestibulum turpis. Praesent bibendum scelerisque nisi, at fermentum diam. Donec lacinia metus id sem feugiat, at tempus ligula mollis. Sed elit purus, iaculis non odio id, malesuada ullamcorper ipsum. Nulla finibus nunc in felis sagittis, vel tincidunt libero rhoncus. In id eleifend eros. Aliquam sit amet nunc eget sem posuere scelerisque eget sit amet massa.</p>
-              <p>Nulla fermentum tortor a mi viverra, at maximus ex lobortis. Vestibulum eget efficitur sem. Aenean ultricies lorem ligula, ac finibus quam sagittis maximus. In ultrices, sapien fermentum rutrum condimentum, erat ligula gravida ante, at bibendum purus nisl at </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="wp-pagenavi">
-      <span class="pages">Page 1 of 8</span>
-      <a href="#" class="first">&lt; First</a>
-      <a href="#" class="previouspostslink" rel="prev"></a>
-      <a href="#" class="page smaller">1</a>
-      <span class="current">2</span>
-      <a href="#" class="page larger">3</a>
-      <a href="#" class="page larger">4</a>
-      <a href="#" class="page larger">5</a>
-      <span class="extend">...</span>
-      <a href="#" class="nextpostslink" rel="next"></a>
-      <a href="#" class="last">Last &gt;</a>
-    </div>
-    <section id="quote">
-      <p>The Pro Comp rifle is just what I've been searching for. I don't think I can ever go back to another rifle. The quality and accuracy is unmatchable.</p>
-    </section>
+    <?php endwhile; wp_pagenavi(); endif; ?>
+    <?php get_template_part('partials/quote.php'); ?>
   </div>
 </main>
 <?php get_footer(); ?>
